1973029c6847c9cbbeff53eb15016899281be981
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-08-31 14:47:36 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -62,7 +62,7 @@ export interface Order {
   price_per_unit: number;
   total_amount: number;
   notes?: string | null;
-  status: 'pending' | 'confirmed' | 'processing' | 'shipped' | 'delivered' | 'cancelled' | string;
+  order_status: 'pending' | 'confirmed' | 'processing' | 'shipped' | 'delivered' | 'cancelled' | string;
   created_at?: string;
 }
 
